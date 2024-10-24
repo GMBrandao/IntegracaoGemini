619b94a09a418bb7adbf600783030c23b13949a1
GMBrandao <gabrielmartbrandao@gmail.com>
2024-10-24 10:15:20 -0300

    Add project files.

---
+++ IntegracaoGemini/Controllers/QuestionController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegracaoGemini.Controllers
+{
+    [ApiController]
+    [Route("question")]
+    public class QuestionController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<QuestionController> _logger;
+
+        public QuestionController(ILogger<QuestionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
